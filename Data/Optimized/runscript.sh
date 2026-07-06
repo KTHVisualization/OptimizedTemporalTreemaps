49d6d9fb2dc66be90cd7796e73577bbf6bf13f6b
Son Le Thanh <sonlt@kth.se>
2026-07-06 11:20:29 +0200

    Deploy page

--- Data/Optimized/runscript.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source /home1/adobler/.bashrc
-cd $1
-echo ../../../build/TTMPL "${@:3}" > "$2.log"
-../../../build/TTMPL "${@:3}" > "$2.log"
--- Data/Others/TTKData/README.md
+++
@@ -1,3 +0,0 @@
-# Raw Tree matching data from TTK
-
-Each folder consists of trees files, extracted using [TTK](https://topology-tool-kit.github.io/).
